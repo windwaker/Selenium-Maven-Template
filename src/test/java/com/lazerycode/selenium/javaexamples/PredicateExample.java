ab78606d4cb491810ddf3d1a1fb648530e270afa
colmh <colm.harrington@gmail.com>
2015-06-14 21:52:24 +0100

    value parameterization

--- src/test/java/com/lazerycode/selenium/javaexamples/PredicateExample.java
+++ src/test/java/com/lazerycode/selenium/javaexamples/PredicateExample.java
@@ -3,7 +3,7 @@ package com.lazerycode.selenium.javaexamples;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
-import com.lazerycode.selenium.tests.Employee;
+import com.lazerycode.selenium.tests.Person;
 
 /**
  * Created by colmh on 17/05/2015.
@@ -12,28 +12,68 @@ public class PredicateExample {
 
     public static void main(String[] args) {
 
-        List<Employee> employeeList = Arrays.asList(new Employee(17, "Charlie"),
-                new Employee(24, "Adam"),
-                new Employee(22, "Bob"));
+        List<Person> personList = Arrays.asList(new Person(17, "Charlie"),
+                new Person(24, "Adam"),
+                new Person(22, "Bob"));
 
-        Predicate<Employee> canVote = (e) -> e.getAge() > 21;
-        Predicate<Employee> noBeer = (e) -> e.getAge() < 18;
-        Predicate<Employee> nameStartsWithA = (e) -> e.getName().toLowerCase().startsWith("a");
+        System.out.println("==== Over 21 Method ====");
+        printCanVote(personList);
 
+        System.out.println("==== Under 18 Method ====");
+        printNoBeer(personList);
+
+        Predicate<Person> canVote = (p) -> p.getAge() > 21;
+        Predicate<Person> noBeer = (p) -> p.getAge() < 18;
+        Predicate<Person> nameStartsWithA = (p) -> p.getName().toLowerCase()
+                                                                .startsWith("a");
+//
         System.out.println("\nCan vote: ");
-        // no method needed for printListOfVoters
-        printFilteredList(employeeList, canVote);
-
+        // no method needed for printCanVote
+        printFilteredList(personList, canVote);
+//
         System.out.println("\nCan't have a beer (in Ireland): ");
         // no method needed for printListOfDrinkers
-        printFilteredList(employeeList, noBeer);
-
+        printFilteredList(personList, noBeer);
+//
         // Use default methods, very powerful concept for webpage testing
         System.out.println("\nCan Vote and name starts with the letter A ");
-        printFilteredList(employeeList, canVote.and(nameStartsWithA));
+        printFilteredList(personList, canVote.and(nameStartsWithA));
     }
 
-    public static void printFilteredList(List<Employee> list, Predicate<Employee> condition){
-        list.stream().filter(condition).forEach(e -> System.out.println("\t" + e.toString()));
+    public static void printCanVote(List<Person> list){
+        for (Person p : list){
+            if(p.getAge() > 21){
+                // act on the data
+                System.out.println("\t" + p.toString());
+            }
+        }
+    }
+
+    public static void printNoBeer(List<Person> list){
+        for (Person p : list){
+            if(p.getAge() < 18){
+                // act on the data
+                System.out.println("\t" + p.toString());
+            }
+        }
+    }
+      // value parameterization
+      // new paths to be tested trough code :-(
+//    public static void printByAge(List<Person> list, int ageLimit, Boolean greater, String nameStartsWith){
+//        for (Person e : list){
+//
+//            if(greater == true){
+//
+//            }
+//            if(e.getAge() > limit){
+//                System.out.println("\t" + e.toString());
+//            }
+//        }
+//    }
+
+    public static void printFilteredList(List<Person> list,
+                                         Predicate<Person> condition){
+        list.stream().filter(condition)
+                .forEach(p -> System.out.println("\t" + p.toString()));
     }
 }
